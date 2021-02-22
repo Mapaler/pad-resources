c2b08d1cec3ba59c53124b1643ea04def74edcf3
枫谷剑仙 <mapaler@163.com>
2021-02-22 09:41:40 +0800

    用时间与大小来判断是否需要重新下载，去掉缓慢的以etag存入catch

--- src/downloader/bc.ts
+++ src/downloader/bc.ts
@@ -1,54 +1,57 @@
 import Axios from 'axios';
-import { existsSync, readFileSync, writeFileSync } from 'fs';
+import { existsSync, readFileSync, writeFileSync, statSync, utimesSync} from 'fs';
 import { padStart } from 'lodash';
 import { join } from 'path';
 import { BC } from '../models/bc';
 import { ExtlistEntry } from '../models/extlist';
 import { TEX } from '../models/tex';
 
-function extractETag(etag: string) {
-  return /^(?:W\/)?"([^"]+)"$/.exec(etag)![1];
-}
-
 export async function downloadBc(
   bcPath: string, binPath: string, cachePath: string,
   extlist: string, entry: ExtlistEntry,
 ): Promise<void> {
   const key = `${entry.isCards ? 'cards' : 'mons'}_${padStart(entry.id.toString(), 3, '0')}`;
-
-  const respHead = await Axios.head(`${key}.bc`, { baseURL: extlist });
-  let etag = extractETag(respHead.headers.etag);
-
-  let data: Buffer;
-  if (existsSync(join(cachePath, etag))) {
-    data = await readFileSync(join(cachePath, etag));
+  //这个文件的最终修改时间
+  const lastUpdate = new Date((entry.lastUpdate + 16305120) * 60 * 1000);
+  const bcFileName = join(bcPath, `${key}.bc`);
+  
+  let data: Buffer, fileExist: Boolean = false;
+  if (existsSync(bcFileName)) {
+    const fileStat = statSync(bcFileName);
+    fileExist = fileStat.mtime >= lastUpdate && fileStat.size == entry.compressedSize;
+  }
+  if (fileExist) {
+    //console.log(`${key} 已经存在旧的`);
+    data = await readFileSync(bcFileName);
+    utimesSync(bcFileName, lastUpdate, lastUpdate);
   } else {
+    console.log(`Start Download ${key}.bc`);
     const resp = await Axios.get(`${key}.bc`, {
       baseURL: extlist,
       responseType: 'arraybuffer',
     });
-    etag = extractETag(resp.headers.etag);
     data = resp.data;
-    await writeFileSync(join(cachePath, etag), data);
-  }
+    await writeFileSync(bcFileName, data);
+    utimesSync(bcFileName, lastUpdate, lastUpdate);
 
-  await writeFileSync(join(bcPath, `${key}.bc`), data);
-
-  const bc = BC.load(data);
-  let binData = bc.data;
-  if (TEX.match(binData)) {
-    // upgrade TEX1 to TEX2 for simpler rendering
-    const tex = TEX.load(binData);
-    if (!tex.info) {
-      tex.info = {
-        cardWidth: entry.width,
-        cardHeight: entry.height,
-        numFrames: entry.numFrames,
-        frameRate: entry.frameRate,
-      };
+    //保存解包的bin
+    const bc = BC.load(data);
+    let binData = bc.data;
+    if (TEX.match(binData)) {
+      // upgrade TEX1 to TEX2 for simpler rendering
+      const tex = TEX.load(binData);
+      if (!tex.info) {
+        tex.info = {
+          cardWidth: entry.width,
+          cardHeight: entry.height,
+          numFrames: entry.numFrames,
+          frameRate: entry.frameRate,
+        };
+      }
+      binData = TEX.save(tex);
     }
-    binData = TEX.save(tex);
+  
+    await writeFileSync(join(binPath, `${key}.bin`), binData);
   }
 
-  await writeFileSync(join(binPath, `${key}.bin`), binData);
 }
